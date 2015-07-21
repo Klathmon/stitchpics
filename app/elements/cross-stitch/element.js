125c3488f5480b037a24c44bda012484cd785f70
Klathmon <gregbenner1@gmail.com>
2015-07-21 13:07:03 -0400

    get rid of old vars and stuff

--- app/elements/cross-stitch/element.js
+++ app/elements/cross-stitch/element.js
@@ -39,7 +39,6 @@
       this.$.finalOutput.webkitImageSmoothingEnabled = false;
 
       var numberOfCores = navigator.hardwareConcurrency || 4;
-      var workerScript;
 
       this.createWorkers('libs.js', numberOfCores);
 
@@ -49,7 +48,6 @@
 
     newFile() {
       this.startTime = performance.now();
-      console.log(this.numcolors);
 
       // First, scale the image correctly
       this.scale({
@@ -120,72 +118,5 @@
       return realImageData;
     },
 
-    oldNewFile() {
-      this.startTime = performance.now();
-      // Scale the image to the correct size
-      var scaledImageData = this.scale(this.imageData, Polymer.dom(this).node.offsetWidth);
-
-      // Get the fitObj
-      var fitObj = this.fit(scaledImageData, this.gridwidth);
-
-      //this.split(scaledImageData, this.workers.length, fitObj).forEach(function(chunk, index){
-
-      //});
-
-      // Build the palette
-      var rgbq = this.buildPalette(scaledImageData, this.numcolors);
-
-      // Break the image into chunks
-      var scaledChunks = this.split(scaledImageData, this.workers.length, fitObj);
-
-      var doneChunks = [];
-
-      scaledChunks.forEach(function(chunk, index) {
-        var quantizeData = {
-          imageDataBuffer: chunk.data.buffer,
-          imageWidth: chunk.width,
-          imageHeight: chunk.height,
-          palette: rgbq.palette(true),
-          numColors: this.numcolors,
-          index: index
-        };
-
-        this.dispatchWorker(function(event) {
-          var pixelateData = {
-            imageDataBuffer: event.data.imageDataBuffer,
-            imageWidth: event.data.imageWidth,
-            imageHeight: event.data.imageHeight,
-            fitObj: fitObj,
-            index: event.data.index
-          };
-
-          this.dispatchWorker(function(event) {
-
-            var doneImageData = workerBehavior.buildImageDataFromBuffer(
-              event.data.imageDataBuffer,
-              event.data.imageWidth,
-              event.data.imageHeight
-            );
-
-            // Done everything for this chunk...
-            doneChunks[event.data.index] = doneImageData;
-
-            var numberOfChunksDone = doneChunks.filter(function(value) {
-              return value !== undefined;
-            }).length;
-
-            if (numberOfChunksDone === scaledChunks.length) {
-              //If we get here we are completely done!!!
-
-              this.stitch(doneChunks, this.$.finalOutput);
-              this.endTime = performance.now();
-
-
-              console.log('Done everything in ' + (this.endTime - this.startTime) + ' milliseconds!');
-            }
-          }, 'pixelate', pixelateData, [pixelateData.imageDataBuffer]);
-        }, 'quantize', quantizeData, [quantizeData.imageDataBuffer]);
-      }, this);
-    }
   });
 })();

0d4b91bde50f9ab4e0464193d85645b1d268cf91
Klathmon <gregbenner1@gmail.com>
2015-08-12 16:50:19 -0400

    added check to only show ads on the live hostname

--- app/elements/my-ad/element.js
+++ app/elements/my-ad/element.js
@@ -19,18 +19,20 @@
     },
 
     ready(){
-      let ins = document.createElement('ins');
-      ins.dataset.adClient = this.client;
-      ins.dataset.adSlot = this.slot;
-      ins.dataset.adFormat = this.format;
-      ins.style.display = 'block';
-      ins.classList.add('adsbygoogle');
-      Polymer.dom(this.root).appendChild(ins);
+      if(window.location.hostname.toLowerCase() === 'stitchpics.net'){
+        let ins = document.createElement('ins');
+        ins.dataset.adClient = this.client;
+        ins.dataset.adSlot = this.slot;
+        ins.dataset.adFormat = this.format;
+        ins.style.display = 'block';
+        ins.classList.add('adsbygoogle');
+        Polymer.dom(this.root).appendChild(ins);
 
-      let script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.appendChild(document.createTextNode('(adsbygoogle = window.adsbygoogle || []).push({});'));
-      Polymer.dom(this.root).appendChild(script);
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.appendChild(document.createTextNode('(adsbygoogle = window.adsbygoogle || []).push({});'));
+        Polymer.dom(this.root).appendChild(script);
+      }
     }
   });
 })();

92d57ca146dbc3eee918773dc63833bd3a7032ac
Klathmon <gregbenner1@gmail.com>
2015-08-20 20:31:53 -0400

    create element tests for my-paper-material

---
+++ app/elements/my-paper-material/tests/element.js
@@ -0,0 +1,21 @@
+suite('Element Tests', ()=> {
+  'use strict';
+  let element;
+
+  setup(()=> element = fixture('basic'));
+
+  test('Elevation is set', ()=> {
+    expect(element.elevation).to.equal('4');
+  });
+
+  test('Elevation matches inner element', ()=> {
+    let innerPaperMaterial = element.querySelector('paper-material');
+    expect(element.elevation).to.equal(innerPaperMaterial.elevation);
+  });
+
+  test('Content matches expected', ()=> {
+    let innerH1 = element.querySelector('h1');
+    expect(innerH1.innerHTML).to.equal('This is test stuff!');
+  });
+
+});
